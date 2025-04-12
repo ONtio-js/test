c0abaa1755aa02f491ee2df2be1ed1a96632766e
Nkwuda Theophilus <theophilusonyebuchinkwuda@gmail.com>
2025-04-12 23:09:03 +0100

    initial commit

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -28,7 +28,7 @@ const Button = ({
       className={`rounded-[32px] bg-primaryColor text-sm lg:text-base ${style} ${variant == "secondary" ? `border border-primaryColor bg-transparent px-3 text-primaryColor transition-all duration-300 ease-in-out  lg:px-4 ${type == "link" ? "py-2.5" : "py-2"} ` : "border-primaryColor px-5 py-3 text-white transition-all duration-300 ease-in-out hover:border hover:bg-white hover:text-primaryColor lg:px-10"} ${secondary ? "flex items-center justify-center gap-1" : ""}`}
       onClick={() => {
         if (type === "link") {
-          router.push(url || "");
+          router.push(url??"");
         } else {
           onclick?.();
         }
